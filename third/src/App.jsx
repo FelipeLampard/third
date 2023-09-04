40031da7d2e4139f97668ca39a3672bc574a0e1a
Jotape <juanpa1999a@gmail.com>
2023-09-03 23:23:35 -0300

    Cambios generales: se eliminan estilizaciones CSS originales, se agregan estilizaciones con bootstrap y se eliminan elementos

--- third/src/App.jsx
+++ third/src/App.jsx
@@ -5,10 +5,12 @@ import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Listado from "./components/Listado";
 import Buscador from "./components/Buscador";
-import Alert from "./components/Alert";
+import Alerta from "./components/Alert";
 
 function App({}) {
+  //Convertir el listado de JS Vanilla a un useState Base para el listado
   const [note, setNote] = useState(bs);
+  //Lo mismo, pero que aplique al agregar un nombre
   const [noteFilter, setNoteFilter] = useState(note);
 
   const [alert, addAlert] = useState({
@@ -21,16 +23,13 @@ function App({}) {
     <>
       <h1 className="mb-3">Lista de colaboradores</h1>
 
-      <div>
+      <div className="container">
         <Buscador note={note} noteFilter={setNoteFilter} />
-        <Listado
-          note={note}
-          setNote={setNote}
-          noteFilter={noteFilter}
-          setNoteFilter={setNoteFilter}
-        />
-      <Formulario addAlert={addAlert} note={note} setNote={setNote} />
-      {alert.estado && <Alert color={alert.color} msg={alert.msg} />}
+        <div className="row">
+          <Listado noteFilter={noteFilter}/>
+          <Formulario addAlert={addAlert} note={note} setNote={setNote} setNoteFilter={setNoteFilter}/>
+        </div>
+        {alert.estado && <Alerta color={alert.color} msg={alert.msg} />}
       </div>
     </>
   );
